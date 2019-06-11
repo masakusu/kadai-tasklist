7bd12a877f2a569c7acc22e2b54a7b5b3db60690
masashi.kusunoki <fatemk73@icloud.com>
2019-06-11 09:53:27 +0000

    perfect

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -74,10 +74,14 @@ class TasksController extends Controller
     public function show($id)
     {
         $task = Task::find($id);
-
-        return view('tasks.show', [
+        
+        if (\Auth::id() === $task->user_id) {
+            return view('tasks.show', [
             'task' => $task,
         ]);
+        }
+        
+        return redirect('/');
     }
 
     /**
@@ -89,10 +93,14 @@ class TasksController extends Controller
     public function edit($id)
     {
         $task = Task::find($id);
-
-        return view('tasks.edit', [
+        
+        if (\Auth::id() === $task->user_id) {
+            return view('tasks.edit', [
             'task' => $task,
         ]);
+        }
+        
+        return redirect('/');
     }
 
     /**
--- app/Task.php
+++ app/Task.php
@@ -2,9 +2,33 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Task extends Model
+class Task extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
